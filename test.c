8a1eb681d31acb6ff08b97d43ff2f5cd1355b98d
majoshi1990 <sparsh123_321@yahoo.com>
2018-12-04 12:19:09 +0530

    modified test.c

--- test.c
+++ test.c
@@ -1,1 +1,1 @@
-#include <stdio.h>int main(){   // printf() displays the string inside quotation   printf("Hello, World!");   return 0;}
+#include <stdio.h>int main(){   printf("Hello, World!");   return 0;}
